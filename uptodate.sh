08b06678a2b2dd9232c6fd3abdf8ba6d8c93a18f
Animesh Kumar Singh <animesh.2001singh@gmail.com>
2021-03-14 18:12:37 +0530

    Final commit every thing working fine now

---
+++ uptodate.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+git_checkout_and_pull()
+{
+    RESULT="$(git checkout $1)"
+        
+    # Pull branch
+    # pull = fetch + push
+    if [[ "$RESULT" == "Your branch is behind"* ]]
+    then
+        echo "PULLING $1"
+        git pull
+    else
+        echo "branch '$1' is up-to-date"
+    fi
+}
+
+clear
+
+PROJECTS_DIR=~/Projects
+
+# Access folder where all projects are saved
+cd $PROJECTS_DIR
+
+# List all folders that contain .git
+DIRS="$(find . -name ".git" | sed 's/\.//g' | sed 's/git//g')"
+
+# Iterate over folders array
+for DIR in $DIRS
+do  
+    cd $PROJECTS_DIR$DIR
+
+    echo "====================================================================="
+    echo "Project: $PROJECTS_DIR$DIR"
+
+    CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+    BRANCHES="$(git branch | sed 's/\*//g')"
+
+    # Iterate over all branches
+    for BRANCH in $BRANCHES
+    do
+        if [[ "$BRANCH" != "$CURRENT_BRANCH" ]]
+        then
+            git_checkout_and_pull $BRANCH    #function call
+        fi     
+    done
+
+    git_checkout_and_pull $CURRENT_BRANCH
+done
+
+exit 0
